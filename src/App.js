d7cf4a2ada47d35da0c96a856763e7b18eb9fc1d
Vinayak Rajguru <vinayak5192@gmail.com>
2023-12-31 10:38:09 +0530

    Adding react graph ql project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import { CountryList } from './Component/CountryList';
 
 function App() {
   return (
     <div className="App">
       <h1>React Component</h1>
       <i className='fa fa-chevron-right'></i>
+      <CountryList></CountryList>
     </div>
   );
 }
---
+++ src/Component/CountryList.js
@@ -0,0 +1,9 @@
+import { Fragment } from "react";
+
+export function CountryList (){
+    return(
+        <Fragment>
+            <h4>Country List:</h4>
+        </Fragment>
+    )
+}
